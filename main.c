b81dc817494e7308ab4fd8e1670a88800ef30f0a
vivianu2001 <118671563+vivianu2001@users.noreply.github.com>
2023-01-15 20:37:38 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "graph.h"
+
+int main(){
+    pnode node = NULL;
+    pnode *head = &node;
+    char c= 'v';
+    while (scanf("%c", &c) != EOF)
+    {
+        if (c == 'A'){
+            build_graph_cmd(head);
+        }
+        else if (c == 'B'){
+            insert_node_cmd(head);
+        }
+        else if (c == 'D'){
+            delete_node_cmd(head);
+        }
+        else if (c == 'S'){
+            int src = -1;
+            int dest = -1;
+            scanf("%d, %d\n", &src, &dest);
+            int shortest = shortsPath_cmd(*head,src,dest);
+            printf("Dijsktra shortest path: %d \n", shortest);
+        }
+        else if (c == 'T'){
+            int tsp = TSP_cmd(*head);
+            printf("TSP shortest path: %d \n", tsp);
+        }
+    }
+    deleteGraph_cmd(head);
+    return 0;
+}
