31bab4e7397b4db52b4838ca08a16c5530195144
vivianu2001 <118671563+vivianu2001@users.noreply.github.com>
2023-01-19 10:57:02 +0200

    Update graph.c

--- graph.c
+++ graph.c
@@ -141,7 +141,7 @@ void insert_node_cmd(pnode *head){
         (*insert_edge)->next = NULL;
        insert_edge = &((*insert_edge)->next);
             
-        free(*insert_edge);   //last commit
+      //  free(*insert_edge);   //last commit
         
     }
 //free(*insert_edge);
